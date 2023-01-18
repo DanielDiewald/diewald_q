638d9ac90cc6ee185cc5391e5adc6eddb9974999
Daniel Diewald <diewald.d02@htlwienwest.at>
2023-01-18 11:05:44 +0100

    bug fixes

--- src/views/EssentialOilsView.vue
+++ src/views/EssentialOilsView.vue
@@ -4,8 +4,8 @@
   <q-responsive :ratio="16 / 9">
     <div class="row justify-center">
       <div class="daniels_banner relative-position">
-        <div class="absolute-center overlay gt-xs" style="text-align: center">
-          <h5><strong>WHAT ARE ESSENTIAL OILS?</strong></h5>
+        <div class="overlay gt-xs" style="text-align: center">
+          <p class="text-h2"><strong>WHAT ARE ESSENTIAL OILS?</strong></p>
           <p>
             The purest distillation of nature, essential oils have enhanced
             lives for thousands of years, and new uses and benefits are still
@@ -15,24 +15,11 @@
       </div>
     </div></q-responsive
   >
-  <div class="row">
-    <div class="col-6 col-xs-12 col-sm-6 daniels_container">
+  <div class="row justify-bewtween items-center">
+    <div class="col-6 col-xs-12 col-sm-6 daniels_container .self-center">
       <div class="relative-position" style="height: 100%; widht: 100%">
-        <div class="absolute-center gt-sm" style="widht: 100%">
-          <h5><strong>Experience the Benefits</strong></h5>
-          <p>
-            Essential oils are concentrated extracts harvested from plants,
-            herbs, and trees, which are carefully steam distilled or cold
-            pressed. These pure, precious oils are more potent than the
-            botanicals from which they’re extracted and contain powerful,
-            concentrated health and wellness benefits. <br /><br />
-            Whether you use oils for aromatherapy, personal care, or as
-            household solutions, all aspects of your life will get a boost with
-            a few drops of essential oils!
-          </p>
-        </div>
-        <div class="lt-md">
-          <h5><strong>Experience the Benefits</strong></h5>
+        <div class="">
+          <p class="text-h5"><strong>Experience the Benefits</strong></p>
           <p>
             Essential oils are concentrated extracts harvested from plants,
             herbs, and trees, which are carefully steam distilled or cold
@@ -64,6 +51,11 @@
   background: white;
   padding: 20px;
   border-radius: 20px;
+  top: 30px;
+  left: 30px;
+  right: 30px;
+  max-width: 50rem;
+  position: absolute;
 }
 .daniels_container {
   padding: 20px;
